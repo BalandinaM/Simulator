77e186c1cc655ba2d9baabd48041df9df5bf59eb
BalandinaM <marina.balandina.89@mail.ru>
2025-03-08 00:16:14 +0500

    удаление слова и списка

--- my-app/src/parts/tableAllWords/tableAllWords.jsx
+++ my-app/src/parts/tableAllWords/tableAllWords.jsx
@@ -1,18 +1,15 @@
-import styles from './tableAllWords.module.css';
-import { useLoaderData } from 'react-router-dom';
-import { getWords } from '../../forStorage';
-import { useState } from 'react';
-import RadioButtonGroup from '../../components/radioButtonGroup/radioButtonGroup';
+import styles from "./tableAllWords.module.css";
+import { useLoaderData } from "react-router-dom";
+import { getWords } from "../../forStorage";
+import { useState } from "react";
+import RadioButtonGroup from "../../components/radioButtonGroup/radioButtonGroup";
 import { produce } from "immer";
+import { findIndexById, startEditing, updateWordState, disablePreviousEditing } from "./handleTableActions";
 
 export async function loader() {
 	const words = await getWords();
-	return {words};
+	return { words };
 }
-
-// Нужно реализовать страницу, на которой будет выводиться таблица всех слов. Нужно реализовать возможность редактирования и удаления слов.
-
-// Нужно сделать переключатель, с помощью которого можно вывести либо все слова, либо слова, которые остались для заучивания, либо уже выученные слова.
 
 const TableAllWords = () => {
 	const { words } = useLoaderData();
@@ -25,111 +22,71 @@ const TableAllWords = () => {
 		setValueRadio(event.target.value);
 	};
 
-	const handleChange = (wordId, event) => {
-		console.log(wordId)
-		let newValue = event.target.value;
-		return newValue;
+	if (valueRadio === 'toLearn' ) {
+		console.log('выучить!!!!')
+	} else if (valueRadio === 'learned') {
+		console.log('ЗНАЮ')
+	} else if (valueRadio === 'all') {
+		console.log('хочу видеть все')
 	}
 
-	const handleClickDateTable = (id) => {// обработка двойного клика по яйчейке таблицы
-		if (isEditing) {// если состояние isEditing
-			console.log("editing....");
-			let indexPrevEdit = wordsState.findIndex((word) => word.isEdit === true); //ищем элемент с isEdit: true
-			if (indexPrevEdit !== -1) {
-				setWordsState(
-					produce((draft) => {
-						draft[indexPrevEdit].isEdit = false; //меняем на false
-					})
-				);
-			}
-			setIsEditing(true); // снова устанавливаем isEditing уже для текущего элемента
-			let index = wordsState.findIndex((word) => word.id === id); // ищем его
-			if (index !== -1) {
-				setWordsState(
-					produce((draft) => {
-						draft[index].isEdit = true; // меняем в стейте значение isEdit: true
-					})
-				);
-			}
-		} else {// если в данный момент ни какие поля не редактируются
-			setIsEditing(true);
-			let index = wordsState.findIndex((word) => word.id === id);
-			if (index !== -1) {
-				setWordsState(
-					produce((draft) => {
-						draft[index].isEdit = true;
-					})
-				);
-			}
+	const handleChange = (wordId, event) => {
+		let newValue = event.target.value;
+		return newValue;
+	};
+
+	const handleClickDateTable = (id) => {
+		if (isEditing) {
+			//отключаем редактирование предыдущего элемента
+			disablePreviousEditing(wordsState, setWordsState);
+			// Включаем редактирование для текущего элемента
+			startEditing(id, setWordsState, wordsState, setIsEditing);
+		} else {
+			// Если редактирование не активно, включаем его для текущего элемента
+			startEditing(id, setWordsState, wordsState, setIsEditing);
 		}
-	};  // эта куча кода написана для того чтобы избежать ситуации в которй юзер кликнул по полю, но ни чего в нем не сделал(курсор там не побывал), соответсвенно события onBlur или Enter, уже не сработают
+	};
 
-
-
-	const handleClickDelete = (event) => {
-		console.log(event.target);
+	const handleClickDelete = (id) => {
+		let index = findIndexById(wordsState, id);
+		if (index !== -1) {
+			setWordsState(
+				produce((draft) => {
+					return draft.filter(elem => {
+						return elem.id != id
+					})
+				})
+			);
+		}
 	};
 
 	const handleBlur = (wordId, parentId) => {
-		console.log("Курсор покинул поле ввода!", wordId);
-		console.log('родительский ид', parentId);
-		if (isEditing) {// если состояние isEditing
-			console.log("editing....");
-			let indexPrevEdit = wordsState.findIndex((word) => word.isEdit === true); //ищем элемент с isEdit: true
-			if (indexPrevEdit !== -1) {
-				setWordsState(
-					produce((draft) => {
-						draft[indexPrevEdit].isEdit = false; //меняем на false
-					})
-				);
-			}}
-
-		let newValue = handleChange(wordId,event);
-		let index = wordsState.findIndex((word) => word.id === parentId);
-			if (index !== -1) {
-				setWordsState(
-					produce((draft) => {
-						let changeElem = Object.values(draft[index]).find(
-							(item) => item.idEng === wordId || item.idRus === wordId
-						);
-						if (changeElem.eng) {
-							changeElem.eng = newValue;
-						} else {
-							changeElem.rus = newValue;
-						}
-					})
-				);
-			}
+		if (isEditing) {
+			disablePreviousEditing(wordsState, setWordsState);
+		}
+		let newValue = handleChange(wordId, event);
+		let index = findIndexById(wordsState, parentId);
+		if (index !== -1) {
+			setWordsState(
+				produce((draft) => {
+					updateWordState(draft, index, wordId, newValue);
+				})
+			);
+		}
 	};
 
 	const handleKeyDown = (wordId, parentId, event) => {
 		if (event.key === "Enter") {
 			console.log("Нажата клавиша Enter!", wordId, parentId);
 			if (isEditing) {
-				// если состояние isEditing
-				console.log("editing....");
-				let indexPrevEdit = wordsState.findIndex((word) => word.isEdit === true); //ищем элемент с isEdit: true
-				if (indexPrevEdit !== -1) {
-					setWordsState(
-						produce((draft) => {
-							draft[indexPrevEdit].isEdit = false; //меняем на false
-						})
-					);
-				}
+				disablePreviousEditing(wordsState, setWordsState);
 			}
 			let newValue = handleChange(wordId, event);
-			let index = wordsState.findIndex((word) => word.id === parentId);
+			let index = findIndexById(wordsState, parentId);
 			if (index !== -1) {
 				setWordsState(
 					produce((draft) => {
-						let changeElem = Object.values(draft[index]).find(
-							(item) => item.idEng === wordId || item.idRus === wordId
-						);
-						if (changeElem.eng) {
-							changeElem.eng = newValue;
-						} else {
-							changeElem.rus = newValue;
-						}
+						updateWordState(draft, index, wordId, newValue);
 					})
 				);
 			}
@@ -191,7 +148,7 @@ const TableAllWords = () => {
 
 								<td>{word.isLearn === false ? <i>выучить</i> : <i>знаю</i>}</td>
 								<td>
-									<button onClick={handleClickDelete}>Удалить</button>
+									<button onClick={() => handleClickDelete(word.id)}>Удалить</button>
 								</td>
 							</tr>
 						))
