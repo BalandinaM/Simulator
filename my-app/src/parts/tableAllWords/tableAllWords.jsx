c259019e0e2d663240dc04b280730776d4d2c9d5
BalandinaM <marina.balandina.89@mail.ru>
2025-03-10 21:39:18 +0500

    строка таблицы вынесена в отдельный компонент

--- my-app/src/parts/tableAllWords/tableAllWords.jsx
+++ my-app/src/parts/tableAllWords/tableAllWords.jsx
@@ -5,6 +5,7 @@ import { useState } from "react";
 import RadioButtonGroup from "../../components/radioButtonGroup/radioButtonGroup";
 import { produce } from "immer";
 import { findIndexById, startEditing, updateWordState, disablePreviousEditing } from "./handleTableActions";
+import TableRow from "../../components/tableRow/tableRow";
 
 export async function loader() {
 	const words = await getWords();
@@ -114,43 +115,15 @@ const TableAllWords = () => {
 				<tbody>
 					{wordsState.length ? (
 						wordsState.map((word) => (
-							<tr key={word.id}>
-								{!word.isEdit ? (
-									<td onDoubleClick={() => handleClickDateTable(word.id)}>
-										{word.engWord.eng ? word.engWord.eng : <i>No word</i>}
-									</td>
-								) : (
-									<td>
-										<input
-											type="text"
-											defaultValue={word.engWord.eng}
-											onBlur={() => handleBlur(word.engWord.idEng, word.id)}
-											onKeyDown={() => handleKeyDown(word.engWord.idEng, word.id, event)}
-											onChange={() => handleChange(word.engWord.idEng, event)}
-										/>
-									</td>
-								)}
-								{!word.isEdit ? (
-									<td onDoubleClick={() => handleClickDateTable(word.id)}>
-										{word.rusWord.rus ? word.rusWord.rus : <i>No word</i>}
-									</td>
-								) : (
-									<td>
-										<input
-											type="text"
-											defaultValue={word.rusWord.rus}
-											onBlur={() => handleBlur(word.rusWord.idRus, word.id)}
-											onKeyDown={() => handleKeyDown(word.rusWord.idRus, word.id, event)}
-											onChange={() => handleChange(word.rusWord.idRus, event)}
-										/>
-									</td>
-								)}
-
-								<td>{word.isLearn === false ? <i>выучить</i> : <i>знаю</i>}</td>
-								<td>
-									<button onClick={() => handleClickDelete(word.id)}>Удалить</button>
-								</td>
-							</tr>
+							<TableRow
+                key={word.id}
+                word={word}
+                handleClickDateTable={handleClickDateTable}
+                handleBlur={handleBlur}
+                handleKeyDown={handleKeyDown}
+                handleChange={handleChange}
+                handleClickDelete={handleClickDelete}
+              />
 						))
 					) : (
 						<tr>
