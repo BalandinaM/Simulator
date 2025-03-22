524a433ce6957ceecd8ab8eeb70e66a6aaba61a4
BalandinaM <marina.balandina.89@mail.ru>
2025-03-23 00:10:51 +0500

    Стилизация блока смены направления перевода

--- my-app/src/components/translationDirectionSwitcher/translationDirectionSwitcher.jsx
+++ my-app/src/components/translationDirectionSwitcher/translationDirectionSwitcher.jsx
@@ -3,11 +3,11 @@ import styles from './translationDirectionSwitcher.module.css';
 const TranslationDirectionSwitcher = ({transIntoRu, setTransIntoRu}) => {
 	return (
 		<div className={styles.wrap_switchTranslete}>
-			{transIntoRu ? <span>Английский</span> : <span>Русский</span>}
+			{transIntoRu ? <span className={styles.rightSpan}>Английский</span> : <span className={styles.rightSpan}>Русский</span>}
 			<button onClick={() => setTransIntoRu(!transIntoRu)}>
 				<img src="arrow_circular.png" alt="Cменить направление перевода" width={25} height={25} />
 			</button>
-			{!transIntoRu ? <span>Английский</span> : <span>Русский</span>}
+			{!transIntoRu ? <span className={styles.leftSpan}>Английский</span> : <span className={styles.leftSpan}>Русский</span>}
 		</div>
 	);
 };
