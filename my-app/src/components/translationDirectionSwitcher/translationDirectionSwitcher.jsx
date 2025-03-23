cd342cd6e6b779d78a21cf4db95fa1d04042eeb9
BalandinaM <marina.balandina.89@mail.ru>
2025-03-23 19:24:42 +0500

    Блокировка кнопки смены направления языка во время тренировки

--- my-app/src/components/translationDirectionSwitcher/translationDirectionSwitcher.jsx
+++ my-app/src/components/translationDirectionSwitcher/translationDirectionSwitcher.jsx
@@ -1,10 +1,10 @@
 import styles from './translationDirectionSwitcher.module.css';
 
-const TranslationDirectionSwitcher = ({transIntoRu, setTransIntoRu}) => {
+const TranslationDirectionSwitcher = ({transIntoRu, setTransIntoRu, isDisabled}) => {
 	return (
 		<div className={styles.wrap_switchTranslete}>
 			{transIntoRu ? <span className={styles.rightSpan}>Английский</span> : <span className={styles.rightSpan}>Русский</span>}
-			<button onClick={() => setTransIntoRu(!transIntoRu)}>
+			<button onClick={() => setTransIntoRu(!transIntoRu)} disabled={!isDisabled}>
 				<img src="arrow_circular.png" alt="Cменить направление перевода" width={25} height={25} />
 			</button>
 			{!transIntoRu ? <span className={styles.leftSpan}>Английский</span> : <span className={styles.leftSpan}>Русский</span>}
