bcf0446dc31ab82c5bd6b6d6e9d42abd71b28dcb
BalandinaM <marina.balandina.89@mail.ru>
2025-03-25 21:32:29 +0500

    Стилизация home

--- my-app/src/parts/home/home.jsx
+++ my-app/src/parts/home/home.jsx
@@ -9,9 +9,9 @@ const Home = () => {
 
 	const renderItem = (arr) => {
 		return arr.map((elem) => (
-			<li key={nanoid(2)}>
-				<h4>{elem.title}</h4>
-				<p>{elem.description}</p>
+			<li className={styles.list_item} key={nanoid(2)}>
+				<h4 className={styles.h4}>{elem.title}</h4>
+				<p className={styles.text}>{elem.description}</p>
 			</li>
 		));
 	};
@@ -21,21 +21,15 @@ const Home = () => {
 
 	return (
 		<section className={styles.container}>
-			{/* <div> */}
-				<h1>{dataHomePage.title}</h1>
-				<p>{dataHomePage.description}</p>
-				<h3>{dataHomePage.advantagesTitle}</h3>
-				<ul className={styles.list}>{advantages}</ul>
-				<h3>{dataHomePage.principlesOfWorkTitle}</h3>
-				<ul className={styles.list}>{principlesOfWork}</ul>
-				<h2>{dataHomePage.motivation.title}</h2>
-				<p>{dataHomePage.motivation.description}</p>
-				<span>{dataHomePage.motivation.slogan}</span>
-				<h3>{dataHomePage.motivationAdd.title}</h3>
-				<p>{dataHomePage.motivationAdd.description}</p>
-				<NavLink to="/signup">{dataHomePage.motivationAdd.textLink}</NavLink>
-				<span>{dataHomePage.slogan}</span>
-			{/* </div> */}
+			<h1 className={styles.h1}>{dataHomePage.title}</h1>
+			<p className={styles.text}>{dataHomePage.description}</p>
+			<h3 className={styles.h3}>{dataHomePage.advantagesTitle}</h3>
+			<ul className={styles.list}>{advantages}</ul>
+			<h3 className={styles.h3}>{dataHomePage.principlesOfWorkTitle}</h3>
+			<ul className={styles.list}>{principlesOfWork}</ul>
+			<h2 className={styles.h2}>{dataHomePage.motivation.title}</h2>
+			<p className={styles.text}>{dataHomePage.motivation.description}</p>
+			<NavLink to="/signup" className={styles.link}>{dataHomePage.motivationAdd.textLink}</NavLink>
 		</section>
 	);
 };
