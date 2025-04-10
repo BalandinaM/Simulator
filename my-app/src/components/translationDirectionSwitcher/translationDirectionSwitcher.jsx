be01548086f76adc081daaec115612a7a49e8e34
BalandinaM <marina.balandina.89@mail.ru>
2025-04-11 00:03:11 +0500

    путь к картинке поправила

--- my-app/src/components/translationDirectionSwitcher/translationDirectionSwitcher.jsx
+++ my-app/src/components/translationDirectionSwitcher/translationDirectionSwitcher.jsx
@@ -1,11 +1,12 @@
 import styles from './translationDirectionSwitcher.module.css';
+import arrow from '../../assets/image/arrow_circular.png'
 
 const TranslationDirectionSwitcher = ({transIntoRu, setTransIntoRu, isDisabled}) => {
 	return (
 		<div className={styles.wrap_switchTranslete}>
 			{transIntoRu ? <span className={styles.rightSpan}>Английский</span> : <span className={styles.rightSpan}>Русский</span>}
 			<button onClick={() => setTransIntoRu(!transIntoRu)} disabled={!isDisabled}>
-				<img src="arrow_circular.png" alt="Cменить направление перевода" width={25} height={25} />
+				<img src={arrow} alt="Cменить направление перевода" width={25} height={25} />
 			</button>
 			{!transIntoRu ? <span className={styles.leftSpan}>Английский</span> : <span className={styles.leftSpan}>Русский</span>}
 		</div>
