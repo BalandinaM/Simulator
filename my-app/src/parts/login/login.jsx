eabd0c72902572dac804932ee71367e60e8315d6
BalandinaM <marina.balandina.89@mail.ru>
2025-04-11 00:21:48 +0500

    Поправила стили логин и регистрации

--- my-app/src/parts/login/login.jsx
+++ my-app/src/parts/login/login.jsx
@@ -6,46 +6,48 @@ import styles from './login.module.css'
 const Login = () => {
 	return (
 			<div className={styles.container}>
-				<h2 className={styles.title}>Авторизация</h2>
-				<Formik
-					initialValues={{ email: "", password: "", rememberMe: false }}
-					validationSchema={createValidationSchema({ email: "", password: "", remembeMe: false })}
-					onSubmit={HandleFormSubmit}
-				>
-					{({ errors, touched, isSubmitting }) => (
-						<Form className={styles.form}>
-							<div className={styles.input_wrap}>
-								<label className={styles.label} htmlFor="email">Адрес электронной почты</label>
-								<Field className={styles.input}
-									type="email"
-									name="email"
-									id="email"
-									autocomplete="email"
-									isinvalid={errors.email && touched.email ? "true" : "false"}
-								/>
-								<ErrorMessage className={styles.error} name="email" component="div" />
-							</div>
-							<div className={styles.input_wrap}>
-								<label className={styles.label} htmlFor="password">Пароль</label>
-								<Field className={styles.input}
-									type="password"
-									name="password"
-									id="password"
-									autocomplete="current-password"
-									isinvalid={errors.password && touched.password ? "true" : "false"}
-								/>
-								<ErrorMessage className={styles.error} name="password" component="div" />
-							</div>
-							<div className={styles.input_wrap}>
-								<label className={styles.label} htmlFor="rememberMe">Запомнить меня</label>
-								<Field className={styles.checkbox} type="checkbox" name="rememberMe" id="rememberMe"/>
-							</div>
-							<button className={styles.button} type="submit" disabled={isSubmitting}>
-								Авторизоваться
-							</button>
-						</Form>
-					)}
-				</Formik>
+				<div className={styles.form_wrap}>
+					<h2 className={styles.title}>Авторизация</h2>
+					<Formik
+						initialValues={{ email: "", password: "", rememberMe: false }}
+						validationSchema={createValidationSchema({ email: "", password: "", remembeMe: false })}
+						onSubmit={HandleFormSubmit}
+					>
+						{({ errors, touched, isSubmitting }) => (
+							<Form className={styles.form}>
+								<div className={styles.input_wrap}>
+									<label className={styles.label} htmlFor="email">Адрес электронной почты</label>
+									<Field className={styles.input}
+										type="email"
+										name="email"
+										id="email"
+										autocomplete="email"
+										isinvalid={errors.email && touched.email ? "true" : "false"}
+									/>
+									<ErrorMessage className={styles.error} name="email" component="div" />
+								</div>
+								<div className={styles.input_wrap}>
+									<label className={styles.label} htmlFor="password">Пароль</label>
+									<Field className={styles.input}
+										type="password"
+										name="password"
+										id="password"
+										autocomplete="current-password"
+										isinvalid={errors.password && touched.password ? "true" : "false"}
+									/>
+									<ErrorMessage className={styles.error} name="password" component="div" />
+								</div>
+								<div className={`${styles.input_wrap} ${styles.wrap_checkbox}`}>
+									<label className={styles.label} htmlFor="rememberMe">Запомнить меня</label>
+									<Field className={styles.checkbox} type="checkbox" name="rememberMe" id="rememberMe"/>
+								</div>
+								<button className={styles.button} type="submit" disabled={isSubmitting}>
+									Авторизоваться
+								</button>
+							</Form>
+						)}
+					</Formik>
+				</div>
 			</div>
 	);
 };
--- my-app/src/parts/signUp/signUp.jsx
+++ my-app/src/parts/signUp/signUp.jsx
@@ -7,77 +7,79 @@ const SignUp = () => {
 
 	return (
 			<div className={styles.container}>
-				<h2 className={styles.title}>Введите данные для создания учетной записи</h2>
-				<Formik
-					initialValues={{ name: "", email: "", password: "", confirmPassword: "" }}
-					validationSchema={
-						Yup.object({
-							name: Yup.string().required('Как к Вам можно обращаться?'),
-							email: Yup.string().email('Неверный формат email').required('Email обязателен'),
-							password: Yup.string()
-								.min(6, 'Минимум 6 символов')
-								.max(20, 'Максимум 20 символов')
-								.required('Пароль обязателен'),
-							confirmPassword: Yup.string()
-								.oneOf([Yup.ref('password'), null], 'Пароли должны совпадать')
-								.required('Подтверждение пароля обязательно'),
-						})
-					}
-					onSubmit={HandleFormSubmit}
-				>
-					{({ errors, touched, isSubmitting }) => (
-						<Form className={styles.form}>
-							<div className={styles.input_wrap}>
-								<label className={styles.label} htmlFor="email">Ваше имя</label>
-								<Field
-									className={styles.input}
-									type="name"
-									name="name"
-									id="name"
-									isinvalid={errors.name && touched.name ? "true" : "false"}
-								/>
-								<ErrorMessage className={styles.error} name="name" component="div" />
-							</div>
-							<div className={styles.input_wrap}>
-								<label className={styles.label} htmlFor="email">Адрес электронной почты</label>
-								<Field
-									className={styles.input}
-									type="email"
-									name="email"
-									id="email"
-									autocomplete="email"
-									isinvalid={errors.email && touched.email ? "true" : "false"}
-								/>
-								<ErrorMessage className={styles.error} name="email" component="div" />
-							</div>
-							<div className={styles.input_wrap}>
-								<label className={styles.label} htmlFor="password">Пароль</label>
-								<Field
-									className={styles.input}
-									type="password"
-									name="password"
-									id="password"
-									isinvalid={errors.password && touched.password ? "true" : "false"}
-								/>
-								<ErrorMessage className={styles.error} name="password" component="div" />
-							</div>
-							<div className={styles.input_wrap}>
-								<label className={styles.label} htmlFor="confirmPassword">Повторите пароль</label>
-								<Field
-									className={styles.input}
-									type="password"
-									name="confirmPassword"
-									id="confirmPassword"
-									isinvalid={errors.confirmPassword && touched.confirmPassword ? "true" : "false"}
-								/>
-								<ErrorMessage className={styles.error} name="confirmPassword" component="div" />
-							</div>
-							<button className={styles.button} type="submit" disabled={isSubmitting}>
-								Зарегистрироваться
-							</button>
-						</Form>
-					)}
-				</Formik>
+				<div className={styles.form_wrap} >
+					<h2 className={styles.title}>Введите данные для создания учетной записи</h2>
+					<Formik
+						initialValues={{ name: "", email: "", password: "", confirmPassword: "" }}
+						validationSchema={
+							Yup.object({
+								name: Yup.string().required('Как к Вам можно обращаться?'),
+								email: Yup.string().email('Неверный формат email').required('Email обязателен'),
+								password: Yup.string()
+									.min(6, 'Минимум 6 символов')
+									.max(20, 'Максимум 20 символов')
+									.required('Пароль обязателен'),
+								confirmPassword: Yup.string()
+									.oneOf([Yup.ref('password'), null], 'Пароли должны совпадать')
+									.required('Подтверждение пароля обязательно'),
+							})
+						}
+						onSubmit={HandleFormSubmit}
+					>
+						{({ errors, touched, isSubmitting }) => (
+							<Form className={styles.form}>
+								<div className={styles.input_wrap}>
+									<label className={styles.label} htmlFor="email">Ваше имя</label>
+									<Field
+										className={styles.input}
+										type="name"
+										name="name"
+										id="name"
+										isinvalid={errors.name && touched.name ? "true" : "false"}
+									/>
+									<ErrorMessage className={styles.error} name="name" component="div" />
+								</div>
+								<div className={styles.input_wrap}>
+									<label className={styles.label} htmlFor="email">Адрес электронной почты</label>
+									<Field
+										className={styles.input}
+										type="email"
+										name="email"
+										id="email"
+										autocomplete="email"
+										isinvalid={errors.email && touched.email ? "true" : "false"}
+									/>
+									<ErrorMessage className={styles.error} name="email" component="div" />
+								</div>
+								<div className={styles.input_wrap}>
+									<label className={styles.label} htmlFor="password">Пароль</label>
+									<Field
+										className={styles.input}
+										type="password"
+										name="password"
+										id="password"
+										isinvalid={errors.password && touched.password ? "true" : "false"}
+									/>
+									<ErrorMessage className={styles.error} name="password" component="div" />
+								</div>
+								<div className={styles.input_wrap}>
+									<label className={styles.label} htmlFor="confirmPassword">Повторите пароль</label>
+									<Field
+										className={styles.input}
+										type="password"
+										name="confirmPassword"
+										id="confirmPassword"
+										isinvalid={errors.confirmPassword && touched.confirmPassword ? "true" : "false"}
+									/>
+									<ErrorMessage className={styles.error} name="confirmPassword" component="div" />
+								</div>
+								<button className={styles.button} type="submit" disabled={isSubmitting}>
+									Зарегистрироваться
+								</button>
+							</Form>
+						)}
+					</Formik>
+				</div>
 			</div>
 	);
 };
